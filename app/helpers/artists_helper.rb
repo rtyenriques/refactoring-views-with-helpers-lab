ebe96ae761a1e51c36ea9f0ace6348ebf6c724f5
= <rtyenriques@gmail.com>
2021-09-28 13:18:20 -0700

    commit tree 4f96e432ce396e548034381aa2d82bc3c23bd760

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,14 @@
 module ArtistsHelper
+
+    def display_artist(a)
+        a
+        if a.artist.nil?
+            link_to "Add Artist", edit_song_path(a)
+        else
+            link_to a.artist.name, artist_path(a.artist)
+        
+        end
+       
+
+    end
 end
